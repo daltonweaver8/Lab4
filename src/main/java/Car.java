65fca1dd8aad9d23629f2230dccf409780e961c4
dalto <dalto@DESKTOP-Q138ALP.hitronhub.home>
2020-04-05 22:38:36 -0500

    Car Class

---
+++ src/main/java/Car.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dalto
+ */
+public class Car {
+    private int yearModel;
+    private String make;
+    private int speed;
+
+    public Car(int yearModel, String make) {
+        this.yearModel = yearModel;
+        this.make = make;
+        this.speed = 0;
+    }
+    public int getYearModel() {
+        return yearModel;
+    }
+    public String getMake() {
+        return make;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void accelerate() {
+        speed += 5;
+    }
+    public void brake() {
+        speed -= 5;
+    }  
+}
